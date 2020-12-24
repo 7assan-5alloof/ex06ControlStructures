6351f2264d0c4fff0a72732a2d91e4462c1346ff
7assan-5alloof <hassankhalloof20@gmail.com>
2020-12-24 10:24:34 +0400

    Make program more compact

--- main.cpp
+++ main.cpp
@@ -10,11 +10,11 @@ int main()
 
     if ((65 <= (int)character) && ((int)character <= 90))
     {
-    cout << "The lower case of " << character << " is " << static_cast<char>((int)character + 32);
+    cout << "The lower case of " << character << " is " << character + 32);
     }
     else if ((97 <= (int)character) && ((int)character <= 122))
     {
-    cout << "The upper case of " << character << " is " << static_cast<char>((int)character - 32);
+    cout << "The upper case of " << character << " is " << character - 32;
     }
     else
     {
